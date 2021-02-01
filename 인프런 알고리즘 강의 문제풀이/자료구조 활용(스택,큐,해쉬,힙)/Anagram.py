a676b735a44c1a781fc9a6fe065fb4c9da346991
임성우 <seongwoorang@naver.com>
2021-02-01 17:46:45 +0900

    Update Anagram.py

--- 인프런 알고리즘 강의 문제풀이/자료구조 활용(스택,큐,해쉬,힙)/Anagram.py
+++ 인프런 알고리즘 강의 문제풀이/자료구조 활용(스택,큐,해쉬,힙)/Anagram.py
@@ -8,10 +8,7 @@ for a in arr:
         print('NO')
         break
 else:
-    if arr2:
-        print('NO')
-    else:
-        print('YES')
+    print('YES')
 
 # 답
 a = input()
@@ -21,7 +18,7 @@ str2 = dict()
 for x in a:
     str1[x] = str1.get(x,0) + 1
 for x in b:
-    str2[x] = str2.get(x,0) + 1
+    str2[x] = str2.get(x,0) + 1     # x라는 키 값이 없으면 0 반환
 for i in str1.keys():
     if i in str2.keys():
         if str1[i] != str2[i]:
@@ -32,3 +29,68 @@ for i in str1.keys():
         break
 else:
     print('YES')
+
+# counter 모듈 사용
+from collections import Counter
+word1 = input()
+word2 = input()
+
+res = Counter(word1) - Counter(word2)
+if len(res) == 0:
+    print('YES')
+else:
+    print('NO')
+
+#개선된 코드
+import sys
+#sys.stdin=open("in1.txt", "r")
+a=input()
+b=input()
+sH=dict()
+for x in a:
+    sH[x]=sH.get(x, 0)+1
+for x in b:
+    sH[x]=sH.get(x, 0)-1
+
+for x in a:
+    if(sH.get(x)>0):
+        print("NO")
+        break;
+else:
+    print("YES")
+
+# 리스트 해쉬
+import sys
+
+a=input()
+b=input()
+str1=[0]*52
+str2=[0]*52
+for x in a:
+    if x.isupper():
+        str1[ord(x)-65]+=1
+    else:
+        str1[ord(x)-71]+=1
+for x in b:
+    if x.isupper():
+        str2[ord(x)-65]+=1
+    else:
+        str2[ord(x)-71]+=1
+for i in range(52):
+    if str1[i]!=str2[i]:
+        print("NO")
+        break
+else:
+    print("YES")
+
+
+
+
+
+
+
+
+
+
+
+
