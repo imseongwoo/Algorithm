3fe2e9dc41cd5f4dd6856e40e399de51464b2095
임성우 <seongwoorang@naver.com>
2021-02-04 18:40:33 +0900

    Create 셸정렬.py

---
+++ 알고리즘/sort/셸정렬.py
@@ -0,0 +1,17 @@
+# 셀 정렬 알고리즘 구현하기
+
+from typing import MutableSequence
+
+def shell_sort(a:MutableSequence) -> None:
+    """셀 정렬"""
+    n = len(a)
+    h = n//2
+    while h>0:
+        for i in range(h,n):
+            j = i-h
+            tmp = a[i]
+            while j >= 0 and a[j] > tmp:
+                a[j+h] = a[j]
+                j -= h
+            a[j+h] = tmp
+        h //= 2
