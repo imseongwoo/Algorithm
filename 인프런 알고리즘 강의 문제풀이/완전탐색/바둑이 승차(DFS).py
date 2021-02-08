a975b349eac98fc4958a97dc0c59ff62a97b8df9
임성우 <seongwoorang@naver.com>
2021-02-08 16:33:11 +0900

    dfs
    cut edge tech

---
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/바둑이 승차(DFS).py
@@ -0,0 +1,51 @@
+def DFS(v,sum):         # 시간초과 발생
+    global temp
+    if sum > c:
+        return
+    if v == n:
+        if sum >temp:
+            temp = sum
+        # ref.append(sum)
+        return
+
+    else:
+        DFS(v+1,sum+arr[v])
+        DFS(v+1,sum)
+
+if __name__=="__main__":
+    c, n = map(int, input().split())
+    arr = []
+    ref = []
+    temp = 0
+    for _ in range(n):
+        arr.append(int(input()))
+    DFS(0,0)
+    print(temp)
+
+# 답
+import sys
+sys.stdin=open("input.txt", "r")
+def DFS(L, sum, tsum):
+    global result
+    if sum+(total-tsum)<result:
+        return
+    if sum>c:
+        return
+    if L==n:
+        if sum>result:
+            result=sum
+    else:
+        DFS(L+1, sum+a[L], tsum+a[L])
+        DFS(L+1, sum, tsum+a[L])
+
+
+if __name__=="__main__":
+    c, n=map(int, input().split())
+    a=[0]*n
+    result=-2147000000
+    for i in range(n):
+        a[i]=int(input())
+    total=sum(a)
+    DFS(0, 0, 0)
+    print(result)
+
