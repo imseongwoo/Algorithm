2660280f02c15729f72852c1c8802dcbadae75c2
임성우 <seongwoorang@naver.com>
2021-03-07 19:27:45 +0900

    Update 중복순열.py
    복습

--- 인프런 알고리즘 강의 문제풀이/완전탐색/중복순열.py
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/중복순열.py
@@ -1,35 +1,42 @@
 def DFS(v):
-    if v
+    global cnt
+    if v==m:
+        for i in range(m):
+            print(res[i],end=' ')
+        print()
+        cnt += 1
+
 
     else:
-        DFS()
-
-
-
+        for i in range(1,n+1):
+            res[v]=i
+            DFS(v+1)
 
 if __name__=="__main__":
 
     n,m = map(int,input().split())
-    DFS(0)
-
-# 답
-import sys
-sys.stdin=open("input.txt", "r")
-def DFS(L):
-    global cnt
-    if L==m:
-        for i in range(m):
-            print(res[i], end=' ')
-        print()
-        cnt+=1
-    else:
-        for i in range(1, n+1):
-            res[L]=i
-            DFS(L+1)
-
-if __name__=="__main__":
-    n, m=map(int, input().split())
-    res=[0]*n
-    cnt=0
+    res = [0]*m
+    cnt = 0
     DFS(0)
     print(cnt)
+# 답
+# import sys
+# sys.stdin=open("input.txt", "r")
+# def DFS(L):
+#     global cnt
+#     if L==m:
+#         for i in range(m):
+#             print(res[i], end=' ')
+#         print()
+#         cnt+=1
+#     else:
+#         for i in range(1, n+1):
+#             res[L]=i
+#             DFS(L+1)
+#
+# if __name__=="__main__":
+#     n, m=map(int, input().split())
+#     res=[0]*n
+#     cnt=0
+#     DFS(0)
+#     print(cnt)
