a1130ab7999d1258093e776479c1458a65c22810
임성우 <seongwoorang@naver.com>
2021-08-06 21:51:15 +0900

    Update 수들의조합(dfs).py
    다른풀이

--- 인프런 알고리즘 강의 문제풀이/완전탐색/수들의조합(dfs).py
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/수들의조합(dfs).py
@@ -14,9 +14,6 @@ def DFS(L,s):
             DFS(L+1,i+1)
 
 
-
-
-
 n,k = map(int,input().split())
 num = list(map(int,input().split()))
 m = int(input())
@@ -25,3 +22,27 @@ cnt = 0
 res = [0]*(n+1)
 DFS(0,0)
 print(cnt)
+
+# 답
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(L, s, sum):
+    global cnt
+    if L == k:
+        if sum % m == 0:
+            cnt += 1
+    else:
+        for i in range(s, n):
+            DFS(L + 1, i + 1, sum + a[i])
+
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    m = int(input())
+    cnt = 0
+    DFS(0, 0, 0)
+    print(cnt)
