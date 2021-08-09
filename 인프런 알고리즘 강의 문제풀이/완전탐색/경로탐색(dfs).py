39d26d583002bcbf72b49596b7c2c2a621af1590
임성우 <seongwoorang@naver.com>
2021-08-09 22:41:47 +0900

    Update 경로탐색(dfs).py
    내 풀이 틀린 코드 확인 및 수정

--- 인프런 알고리즘 강의 문제풀이/완전탐색/경로탐색(dfs).py
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/경로탐색(dfs).py
@@ -9,7 +9,7 @@ def DFS(L):
             if graph[L][i] == 1 and ch[i] == 0:
                 ch[i] = 1
                 DFS(i)
-
+                ch[i] = 0           # 이게 문제였어
 
 
 n,m = map(int,input().split())
@@ -26,38 +26,38 @@ print(cnt)
 
 
 # 답
-import sys
-
-sys.stdin = open("input.txt", "r")
-
-
-def DFS(v):
-    global cnt, path
-    if v == n:
-        cnt += 1
-        for x in path:
-            print(x, end=' ')
-        print()
-    else:
-        for i in range(1, n + 1):
-            if g[v][i] == 1 and ch[i] == 0:
-                ch[i] = 1
-                path.append(i)
-                DFS(i)
-                path.pop()
-                ch[i] = 0
-
-
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-    g = [[0] * (n + 1) for _ in range(n + 1)]
-    ch = [0] * (n + 1)
-    for i in range(m):
-        a, b = map(int, input().split())
-        g[a][b] = 1
-    cnt = 0
-    ch[1] = 1
-    path = list()
-    path.append(1)
-    DFS(1)
-    print(cnt)
+# import sys
+#
+# sys.stdin = open("input.txt", "r")
+#
+#
+# def DFS(v):
+#     global cnt, path
+#     if v == n:
+#         cnt += 1
+#         for x in path:
+#             print(x, end=' ')
+#         print()
+#     else:
+#         for i in range(1, n + 1):
+#             if g[v][i] == 1 and ch[i] == 0:
+#                 ch[i] = 1
+#                 path.append(i)
+#                 DFS(i)
+#                 path.pop()
+#                 ch[i] = 0
+#
+#
+# if __name__ == "__main__":
+#     n, m = map(int, input().split())
+#     g = [[0] * (n + 1) for _ in range(n + 1)]
+#     ch = [0] * (n + 1)
+#     for i in range(m):
+#         a, b = map(int, input().split())
+#         g[a][b] = 1
+#     cnt = 0
+#     ch[1] = 1
+#     path = list()
+#     path.append(1)
+#     DFS(1)
+#     print(cnt)
