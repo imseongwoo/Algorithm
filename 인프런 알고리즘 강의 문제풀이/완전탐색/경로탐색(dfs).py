b9dc0ce6acadcbcb11f60f64eb27bef6b6747b99
임성우 <seongwoorang@naver.com>
2021-08-09 22:36:23 +0900

    Update 경로탐색(dfs).py
    답

--- 인프런 알고리즘 강의 문제풀이/완전탐색/경로탐색(dfs).py
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/경로탐색(dfs).py
@@ -6,24 +6,58 @@ def DFS(L):
 
     else:
         for i in range(1, n + 1):
-            if graph[L][i] == 1 and ch[L][i] == 0:
-                ch[L][i] = 1
-                ch[i][L] = 1
+            if graph[L][i] == 1 and ch[i] == 0:
+                ch[i] = 1
                 DFS(i)
-
-            else:
-                continue
 
 
 
 n,m = map(int,input().split())
 graph = [[0]*(n+1) for _ in range(n+1)]
-ch = [[0]*(n+1) for _ in range(n+1)]
+ch = [0]*(n+1)
 
 cnt = 0
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a][b] = 1
-
+ch[1]=1
 DFS(1)
 print(cnt)
+
+
+# 답
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def DFS(v):
+    global cnt, path
+    if v == n:
+        cnt += 1
+        for x in path:
+            print(x, end=' ')
+        print()
+    else:
+        for i in range(1, n + 1):
+            if g[v][i] == 1 and ch[i] == 0:
+                ch[i] = 1
+                path.append(i)
+                DFS(i)
+                path.pop()
+                ch[i] = 0
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    g = [[0] * (n + 1) for _ in range(n + 1)]
+    ch = [0] * (n + 1)
+    for i in range(m):
+        a, b = map(int, input().split())
+        g[a][b] = 1
+    cnt = 0
+    ch[1] = 1
+    path = list()
+    path.append(1)
+    DFS(1)
+    print(cnt)
