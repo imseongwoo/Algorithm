7c48bfd067aa20e89fff4357006da4c92c9f9c95
임성우 <seongwoorang@naver.com>
2021-08-10 20:20:57 +0900

    Update 최대점수구하기(dfs).py
    답

--- 인프런 알고리즘 강의 문제풀이/완전탐색/최대점수구하기(dfs).py
+++ 인프런 알고리즘 강의 문제풀이/완전탐색/최대점수구하기(dfs).py
@@ -1,14 +1,11 @@
 def DFS(v,sum,t):
     global maxnum
-    if time >m:
+    if t > m:
         return
-    if v==n:
-        # 최대점수구하기
-        # sum = sum - graph[v-1][0]
-        # t = t - graph[v-1][1]
+    if v==n or t>m:
         if sum > maxnum:
             maxnum = sum
-
+        return
     else:
         DFS(v+1,sum+graph[v][0],t+graph[v][1])
         DFS(v+1,sum,t)
@@ -16,12 +13,36 @@ def DFS(v,sum,t):
 
 maxnum = -2147000000
 n,m = map(int,input().split())
-graph = [0]*n
+graph = [0]*(n+1)
 ch = [0]*n
 for i in range(n):
     score,time = map(int,input().split())
     graph[i] = (score,time)
-
 DFS(0,0,0)
 print(maxnum)
-print(graph)
+
+# 답
+import sys
+sys.stdin=open("input.txt", "r")
+def DFS(L, sum, time):
+    global res
+    if time>m:
+        return
+    if L==n:
+        if sum>res:
+            res=sum
+    else:
+        DFS(L+1, sum+pv[L], time+pt[L])
+        DFS(L+1, sum, time)
+
+if __name__=="__main__":
+    n, m=map(int, input().split())
+    pv=list()
+    pt=list()
+    for i in range(n):
+        a, b=map(int, input().split())
+        pv.append(a)
+        pt.append(b)
+    res=-2147000000
+    DFS(0, 0, 0)
+    print(res)
