4bbc87382e64a7f24a55c1a93efbe5d41e5d3e0e
임성우 <seongwoorang@naver.com>
2022-01-12 23:08:33 +0900

    미로 최단거리
    주석 추가

--- 인프런 알고리즘 강의 문제풀이/깊이 넓이 우선탐색 활용/미로 최단거리.py
+++ 인프런 알고리즘 강의 문제풀이/깊이 넓이 우선탐색 활용/미로 최단거리.py
@@ -7,25 +7,24 @@ q.append((0,0))
 check = [[0]*7 for _ in range(7)]
 dist = [[0]*7 for _ in range(7)]
 num = 0
-minum = 9999999
 
-while q:
+while q:        # 큐가 빌 때까지
     temp = q.popleft()
-    if temp[0] == 6 and temp[1] == 6:
+    if temp[0] == 6 and temp[1] == 6:       # 도착지점 도달 시 종료 거리가 1씩 증가하므로 먼저 도달한 경로가 최단경로임.
 
         break
 
 
-    for a in range(4):
+    for a in range(4):      # 상하좌우 반복
         x = temp[0] + dx[a]
         y = temp[1] + dy[a]
 
-        if x >= 0 and y>=0 and x<=6 and y<=6:
-            if arr[x][y] != 1 and check[x][y] == 0:
-                q.append((x,y))
-                check[x][y] = 1
-                dist[x][y] = dist[temp[0]][temp[1]] + 1
-
+        if x >= 0 and y>=0 and x<=6 and y<=6: # 미로 범위내에서
+            if arr[x][y] != 1 and check[x][y] == 0:     # 방문하지 않았고 벽이 아니라면 실행
+                q.append((x,y))     # 큐에 append
+                check[x][y] = 1     # check
+                dist[x][y] = dist[temp[0]][temp[1]] + 1     # 이전경로까지의 거리+1
+#문제 조건
 if dist[6][6] == 0 :
     print('-1')
 else:
