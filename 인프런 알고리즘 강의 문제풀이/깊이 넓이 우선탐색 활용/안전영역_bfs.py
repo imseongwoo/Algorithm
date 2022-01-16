758f29e12e60f2f43edc68629f86962f837d9bb1
임성우 <seongwoorang@naver.com>
2022-01-16 15:21:48 +0900

    안전영역
    BFS

---
+++ 인프런 알고리즘 강의 문제풀이/깊이 넓이 우선탐색 활용/안전영역_bfs.py
@@ -0,0 +1,43 @@
+from collections import deque
+from copy import deepcopy
+
+dx = [-1, 0, 1, 0]              # 상하좌우 방향 벡터
+dy = [0, 1, 0, -1]
+
+res = []
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+maxnum = -1
+
+for a in range(n):          # 최대값 찾기
+    for b in range(n):
+        if arr[a][b] > maxnum:
+            maxnum = arr[a][b]
+
+
+
+for a in range(maxnum):     # 모든 높이에 대해서 반복
+    height = a + 1
+    q = deque()
+    arr2 = deepcopy(arr)       # 2차원 리스트 복사
+    cnt = 0
+    for i in range(n):          # 리스트의 모든 항목에 대해서 반복
+        for j in range(n):
+            if arr2[i][j] > height:     # 안전한 위치면 실행
+                q.append((i,j))
+                arr2[i][j] = 0          # 방문 체크
+
+                while q:
+                    tmp = q.popleft()
+                    for x in range(4):
+                        tx = tmp[0] + dx[x]
+                        ty = tmp[1] + dy[x]
+
+                        if 0 <= tx < n and 0 <= ty < n and arr2[tx][ty] > height:
+                            arr2[tx][ty] = 0
+                            q.append((tx,ty))
+                cnt += 1
+    res.append(cnt)
+
+print(max(res))
