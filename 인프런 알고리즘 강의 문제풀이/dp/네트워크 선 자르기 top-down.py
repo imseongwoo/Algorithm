a0602f785b990379b79055e372a97473559333ee
임성우 <seongwoorang@naver.com>
2022-01-26 18:14:59 +0900

    네트워크 선 자르기
    top-down

---
+++ 인프런 알고리즘 강의 문제풀이/dp/네트워크 선 자르기 top-down.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+def dfs(len):
+    if dy[len] > 0 :
+        return dy[len]
+    if len == 1 or len == 2:
+        return len
+    else:
+        dy[len] = dfs(len-1) + dfs(len-2)
+        return dy[len]
+
+
+
+
+dy = [0] * (n+1)
+print(dfs(n))
