33c948822016ee5729c01825cc0ffd693c60a23b
임성우 <seongwoorang@naver.com>
2022-01-28 15:14:22 +0900

    Create 최대 선 연결하기.py

---
+++ 인프런 알고리즘 강의 문제풀이/dp/최대 선 연결하기.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int,input().split()))
+arr.insert(0,0)
+
+d = [0] * (n+1)
+
+for a in range(1,n+1):
+    temp = 0
+    for b in range(1,a):
+        if arr[a] > arr[b]:
+            temp = max(temp,d[b])
+
+    d[a] = temp + 1
+
+print(max(d))
+
