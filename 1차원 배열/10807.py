102196b8e31659df63d83f4ec6083296e587c760
임성우 <seongwoorang@naver.com>
2022-12-22 17:41:53 +0900

    백준 1차원 배열

---
+++ 1차원 배열/10807.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int,input().split()))
+v = int(input())
+count = 0
+for i in arr:
+    if i == v:
+        count += 1
+
+print(count)
---
+++ 1차원 배열/10871.py
@@ -0,0 +1,6 @@
+n,x = map(int,input().split())
+arr = list(map(int,input().split()))
+
+for a in arr:
+    if a < x :
+        print(a,end=" ")
