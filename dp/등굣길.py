339e3b809587bbddf311164b9534e43ee0fa2d7e
임성우 <seongwoorang@naver.com>
2024-02-17 20:55:13 +0900

    dp level3 다른풀이

--- dp/등굣길.py
+++ dp/등굣길.py
@@ -1,34 +1,14 @@
-from copy import deepcopy
-
-
-def dfs(x, y, check, m, n):
-    global answer
-    dx = [0, 1]
-    dy = [1, 0]
-    if x == (n - 1) and y == (m - 1):
-        answer += 1
-    else:
-        for a in range(2):
-            nx = x + dx[a]
-            ny = y + dy[a]
-
-            if 0 <= nx < n and 0 <= ny < m and check[nx][ny] != 1:
-                check[nx][ny] = 1
-                dfs(nx, ny, check, m, n)
-                check[nx][ny] = 0
-
 
 def solution(m, n, puddles):
-    global answer
+    dp = [[0]*m for _ in range(n)]
+    dp[0][0] = 1
 
-    arr = [[0] * m for _ in range(n)]
-    check = deepcopy(arr)
-    check[puddles[0][0] - 1][puddles[0][1] - 1] = 1
+    for a in range(n):
+        for b in range(m):
+            if ([b+1,a+1] in puddles) or (a==0 and b==0):
+                continue
+            else:
+                dp[a][b] = (dp[a-1][b] + dp[a][b-1]) % 1000000007
+    return dp[-1][-1]
 
-    dfs(0, 0, check, m, n)
-
-    return answer%1000000007
-
-
-answer = 0
-print(solution(4, 3, [[2, 2]]))
+print(solution(4,3,[[2,2]]))
