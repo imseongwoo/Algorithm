d89cd5f378d98aeff74661f95190d3285a491a50
임성우 <seongwoorang@naver.com>
2024-02-23 15:45:46 +0900

    큐

---
+++ 큐,덱/기능개발.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds):
+    answer = []
+
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        if cnt > 0:
+            answer.append(cnt)
+
+    return answer
+
+
+solution([93,30,55], [1,30,5])
