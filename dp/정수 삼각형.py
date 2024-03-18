4ea58705bc0dd46221387c356fde24e552ab1bcb
임성우 <seongwoorang@naver.com>
2024-03-18 16:04:15 +0900

    dp 복습

--- dp/정수 삼각형.py
+++ dp/정수 삼각형.py
@@ -1,17 +1,33 @@
 def solution(triangle):
     height = len(triangle)
-    dp = [[0]*(a+1) for a in range(height)]
+    dp = [[0] * (a + 1) for a in range(height)]
 
     for a in dp:
-        a.insert(0,0)
+        a.insert(0, 0)
         a.append(0)
 
     dp[0][1] = triangle[0][0]
     for i in range(1, height):
-        for j in range(1,i+2):
-            dp[i][j] = triangle[i][j-1] + max(dp[i-1][j],dp[i-1][j-1])
+        for j in range(1, i + 2):
+            dp[i][j] = triangle[i][j - 1] + max(dp[i - 1][j], dp[i - 1][j - 1])
 
     return max(dp[-1])
 
 
-print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+def solution2(triangle):
+    height = len(triangle)
+    dp = [[*a] for a in triangle]
+
+    for a in dp:
+        a.insert(0, 0)
+        a.append(0)
+
+    dp[0][1] = triangle[0][0]
+    for i in range(1, height):
+        for j in range(1, i + 2):
+            dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
+
+    print(max(dp[-1]))
+
+
+print(solution2([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
