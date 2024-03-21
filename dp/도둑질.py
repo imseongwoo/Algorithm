f8cf8f6a947ec26fd37e0c5a04ee75c608377e5c
임성우 <seongwoorang@naver.com>
2024-03-21 13:25:18 +0900

    dp

--- dp/도둑질.py
+++ dp/도둑질.py
@@ -1,10 +1,9 @@
 def solution(money):
-    answer = 0
     dp1 = [0] * len(money)
     dp1[0] = money[0]
     dp1[1] = money[0]
 
-    for i in range(2, len(money)):
+    for i in range(2, len(money)-1):
         dp1[i] = max(dp1[i-1],money[i]+dp1[i-2])
 
     dp2 =[0] * len(money)
