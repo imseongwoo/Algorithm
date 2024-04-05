69b682cd1d369850e424b5e1b1d2b471b4fbd7e7
임성우 <seongwoorang@naver.com>
2024-04-05 16:58:21 +0900

    소수 알고리즘

--- 투포인터/소수의 연속합.py
+++ 투포인터/소수의 연속합.py
@@ -47,3 +47,15 @@ while True:
         sum += prime_list[end]
 
 print(answer)
+
+# 에라토스테네스 체
+def prime_numbers(n):
+    arr = [0] * (n+1)  # 인덱싱을 수월하게 하기 위해 0부터 배열 선언
+    end = int(n ** (1 / 2))
+    for i in range(2, end + 1):
+        if arr[i] == 0:  # 이미 소수가 아님이 판별된 수는 건너뜀
+            continue
+        for j in range(i * i, n + 1, i):  # 자기 자신을 제외한 i의 배수는 모두 0으로 처리함.
+            arr[j] = 0
+
+    return [i for i in arr[2:] if arr[i]]
