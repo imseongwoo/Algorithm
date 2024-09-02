4224215b03b336cc16f6f1b56e84a055b56e8dfa
임성우 <seongwoorang@naver.com>
2024-09-02 21:32:44 +0900

    dp

---
+++ dp/서로 다른 BST 개수 세기.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+UNUSED = -1
+
+memo = [UNUSED for _ in range(n + 1)]
+
+
+def get_num_of_unique_bst(n):
+    if memo[n] != UNUSED:
+        return memo[n]
+
+    if n <= 1:
+        return 1
+
+    number_of_unique_bst = 0
+    for i in range(n):
+        number_of_unique_bst += get_num_of_unique_bst(i) * get_num_of_unique_bst(n - i - 1)
+
+    memo[n] = number_of_unique_bst
+    return memo[n]
+
+
+print(get_num_of_unique_bst(n))
