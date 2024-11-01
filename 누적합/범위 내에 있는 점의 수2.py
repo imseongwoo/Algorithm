15cba172f6ac231ca585427515765a92b3f85c05
임성우 <seongwoorang@naver.com>
2024-11-01 18:05:20 +0900

    누적합 [a,b] ps[b]-ps[a-1]

---
+++ 누적합/범위 내에 있는 점의 수2.py
@@ -0,0 +1,19 @@
+n, q = map(int, input().split())
+arr = list(map(int, input().split()))
+num = 1000001
+check = [0] * num
+prefix_sum = [0] * num
+
+for a in arr:
+    check[a] = 1
+
+for i in range(1, num):
+    prefix_sum[i] = prefix_sum[i-1] + check[i]
+
+for _ in range(q):
+    a, b = map(int, input().split())
+    if a == 0:
+        ans = prefix_sum[b]
+    else:
+        ans = prefix_sum[b] - prefix_sum[a-1]
+    print(ans)
